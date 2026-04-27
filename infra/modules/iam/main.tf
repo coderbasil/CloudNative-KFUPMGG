5210870b606308e157da4293caa12fc14f1f43a1
basil <bsuli1424@gmail.com>
2026-04-27 05:44:44 +0300

    Give GitHub Actions role AdministratorAccess for Terraform

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -61,18 +61,9 @@ resource "aws_iam_role" "github_actions" {
   assume_role_policy = data.aws_iam_policy_document.github_oidc_assume.json
 }
 
-resource "aws_iam_role_policy" "github_actions" {
-  name = "${var.project}-deploy"
-  role = aws_iam_role.github_actions.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      { Effect = "Allow", Action = ["ecr:GetAuthorizationToken"], Resource = "*" },
-      { Effect = "Allow", Action = ["ecr:BatchCheckLayerAvailability", "ecr:InitiateLayerUpload", "ecr:UploadLayerPart", "ecr:CompleteLayerUpload", "ecr:PutImage", "ecr:BatchGetImage", "ecr:GetDownloadUrlForLayer"], Resource = "arn:aws:ecr:${var.aws_region}:${var.account_id}:repository/${var.project}-*" },
-      { Effect = "Allow", Action = ["ssm:SendCommand", "ssm:GetCommandInvocation"], Resource = ["arn:aws:ssm:${var.aws_region}::document/AWS-RunShellScript", "arn:aws:ec2:${var.aws_region}:${var.account_id}:instance/*"] },
-      { Effect = "Allow", Action = ["s3:GetObject", "s3:PutObject", "s3:ListBucket", "dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:DeleteItem"], Resource = ["arn:aws:s3:::kfupm-geo-tf-state", "arn:aws:s3:::kfupm-geo-tf-state/*", "arn:aws:dynamodb:${var.aws_region}:${var.account_id}:table/kfupm-geo-tf-lock"] }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 output "ec2_instance_profile_name" { value = aws_iam_instance_profile.ec2.name }
